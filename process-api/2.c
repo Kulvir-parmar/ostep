30fbc596e1393a7eafac1f4b0bde14879d8c1608
Kulvir Singh <kulvirs734@gmail.com>
2024-08-21 12:47:16 +0530

    3

--- process-api/2.c
+++ process-api/2.c
@@ -1,18 +1,11 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
-/*
-I have no idea about this.
-I wrote a program to write to the same file in both child and parent process.
-Writes of both of them were successful.
- */
-
 int main(int argc, char **argv) {
-    int fd = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    int fd = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC);
+
     if (fd == -1) {
         perror("Error opening file");
         exit(-1);
@@ -22,19 +15,10 @@ int main(int argc, char **argv) {
     if (rc < 0) {
         fprintf(stderr, "fork failed\n");
     } else if (rc == 0) {
-        for (int i = 0; i < 10; i++) {
-            char buffer[50];
-            sprintf(buffer, "Child writing line %d\n", i);
-            write(fd, buffer, strlen(buffer));
-        }
-        printf("(pid: %d)\n", (int)getpid());
+        printf("fd in child: %d\n", fd);
     } else {
-        for (int i = 0; i < 10; i++) {
-            char buffer[50];
-            sprintf(buffer, "Parent writing line %d\n", i);
-            write(fd, buffer, strlen(buffer));
-        }
-        printf("(pid: %d)\n", (int)getpid());
+        printf("fd in parent: %d\n", fd);
+        wait(NULL);
     }
     close(fd);
     return 0;
--- process-api/4.c
+++ process-api/4.c
@@ -20,7 +20,6 @@ int main(int argc, char *argv[]) {
         myargs[2] = NULL;
 
         execvp(myargs[0], myargs);
-        printf("this shouldn't print out\n");
     } else {
         int wc = wait(NULL);
         printf("hello, I am parent of %d (wc:%d) (pid:%d)\n", rc, 0,
